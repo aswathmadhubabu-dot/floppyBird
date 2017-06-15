09fc63388cba2ed72eacb6de6892c66f5f4239f0
ases573 <ases573@gmail.com>
2017-06-15 19:17:15 +0530

    Getting dom elements under variable

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+
+var container = $("#container");
+var bird = $("#bird");
+var pole = $(".pole");
+var pole1 = $("#pole1");
+var pole2 = $("#pole2");
+var score = $("#score");
+var speed_span = $("#speed");
+var restart_btn = $("#restart_btn");
+
+var container_width = parseInt(container.width());
+var pole_initial_position = parseInt(pole.css('right'));
+var pole_initial_height = parseInt(pole.css('height'));
+var bird_left = parseInt(bird.css('left'));
+var speed = 10;
+});
